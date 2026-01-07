30fc3eeaee136bc5053d86dd985b3aa0d27055d1
Saurav Swaraj <saurav@unibcomp.net>
2026-01-07 13:27:19 +0530

    added /github hiteshchoudhary api data

--- index.js
+++ index.js
@@ -1,24 +1,66 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = 4000
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 4000;
+const githubData = {
+  login: "hiteshchoudhary",
+  id: 11613311,
+  node_id: "MDQ6VXNlcjExNjEzMzEx",
+  avatar_url: "https://avatars.githubusercontent.com/u/11613311?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/hiteshchoudhary",
+  html_url: "https://github.com/hiteshchoudhary",
+  followers_url: "https://api.github.com/users/hiteshchoudhary/followers",
+  following_url:
+    "https://api.github.com/users/hiteshchoudhary/following{/other_user}",
+  gists_url: "https://api.github.com/users/hiteshchoudhary/gists{/gist_id}",
+  starred_url:
+    "https://api.github.com/users/hiteshchoudhary/starred{/owner}{/repo}",
+  subscriptions_url:
+    "https://api.github.com/users/hiteshchoudhary/subscriptions",
+  organizations_url: "https://api.github.com/users/hiteshchoudhary/orgs",
+  repos_url: "https://api.github.com/users/hiteshchoudhary/repos",
+  events_url: "https://api.github.com/users/hiteshchoudhary/events{/privacy}",
+  received_events_url:
+    "https://api.github.com/users/hiteshchoudhary/received_events",
+  type: "User",
+  user_view_type: "public",
+  site_admin: false,
+  name: "Hitesh Choudhary",
+  company: null,
+  blog: "https://hitesh.ai",
+  location: "India",
+  email: null,
+  hireable: null,
+  bio: "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out",
+  twitter_username: "hiteshdotcom",
+  public_repos: 116,
+  public_gists: 5,
+  followers: 53550,
+  following: 0,
+  created_at: "2015-03-23T13:03:25Z",
+  updated_at: "2025-12-10T16:57:39Z",
+};
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get("/twitter", (req, res)=> {
-    res.send("sauravswaraj.com")
-})
+app.get("/twitter", (req, res) => {
+  res.send("sauravswaraj.com");
+});
 
-app.get("/login", (req, res)=> {
-    res.send("Welcome to Login Page")
-})
+app.get("/login", (req, res) => {
+  res.send("Welcome to Login Page");
+});
 
-app.get("/youtube", (req, res)=>{
-    res.send("<h2>Saurav Youtube Channel</h2>")
+app.get("/youtube", (req, res) => {
+  res.send("<h2>Saurav Youtube Channel</h2>");
+});
+app.get("/github", (req, res)=>{
+    res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
